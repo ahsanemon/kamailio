89074746c97035940bb6b1288b593428d17fc648
ahsanemon <30293711+ahsanemon@users.noreply.github.com>
2017-11-04 16:21:56 +0100

    Create kamailio-config.sh

---
+++ kamailio-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+  
+# DB IP address
+if  [[ !  -z  $DBHOST ]]; then 
+    printf "\nChanging DB IP address ...\n" 
+    sed -i "s/# DBHOST=localhost/DBHOST=$DBHOST/g" /usr/local/etc/kamailio/kamctlrc 
+    cat /usr/local/etc/kamailio/kamctlrc | grep DBHOST= 
+fi 
+
+# Start the service 
+service kamailio restart
+
+exec "$@" 
