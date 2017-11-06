b1e2bd9fb294a0aed236eaad399a65a7f6f387e1
ahsanemon <30293711+ahsanemon@users.noreply.github.com>
2017-11-06 14:41:04 +0100

    Update kamailio-config.sh

--- kamailio-config.sh
+++ kamailio-config.sh
@@ -2,9 +2,23 @@
   
 # DB IP address
 if  [[ !  -z  $DBHOST ]]; then 
-    printf "\nChanging DB IP address ...\n" 
-    sed -i "s/# DBHOST=localhost/DBHOST=$DBHOST/g" /usr/local/etc/kamailio/kamctlrc 
+    printf "\nUpdating DB IP address to...\n" 
+    sed -i "s/.*DBHOST=.*/DBHOST=$DBHOST/g" /usr/local/etc/kamailio/kamctlrc 
     cat /usr/local/etc/kamailio/kamctlrc | grep DBHOST= 
 fi 
+
+# DB root access
+if  [[ !  -z  $DBHOST ]]; then 
+    printf "\nUpdating DB root user and pass...\n" 
+    sed -i 's/.*DBROOTUSER=.*/DBROOTUSER="$DBROOTUSER" \&\& PW="$DBROOTPASS"/g' /usr/local/etc/kamailio/kamctlrc
+    cat /usr/local/etc/kamailio/kamctlrc | grep DBROOTUSER= 
+fi 
+
+# SIP Domain
+if  [[ !  -z  $SIP_DOMAIN ]]; then 
+    printf "\nUpdating SIP Domain to...\n" 
+    sed -i "s/.*SIP_DOMAIN=.*/SIP_DOMAIN=$SIP_DOMAIN/g" /usr/local/etc/kamailio/kamctlrc 
+    cat /usr/local/etc/kamailio/kamctlrc | grep SIP_DOMAIN= 
+fi
 
 exec "$@" 
